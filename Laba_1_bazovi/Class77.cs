b60ac9ba417812ee20d0d6c7d57a1dceafe678ce
Serj Tigranyan <serj-tigranyan@mail.ru>
2022-06-28 17:11:47 +0200

    Добавьте файлы проекта.

---
+++ Laba_1_bazovi/Class77.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1_bazovi
+{
+    internal class Class77
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public Class77(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double Result()
+        {
+            return A * B;
+        }
+    }
+}
